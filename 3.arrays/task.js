f4d9080ce47eaa818291e38f33c62d40700a48b7
Ольга Копылова <kopylova_olya@mail.ru>
2023-04-14 22:08:07 +0300

    Задачи по теме массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+      }
+      return arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let filteredUsers = users.filter(user => user.gender === gender);
+  if (filteredUsers.length === 0) { 
+    return 0;
+  }
+  let agesSum = filteredUsers.map(user => user.age).reduce((sum, age) => sum + age);
+  return agesSum / filteredUsers.length;
 }
